0e07c09691c2bb5ac3ba2edcb8a07ec69ff9bc71
Talha Alkan <alkantalha10@gmail.com>
2022-09-03 20:56:32 +0300

    bir düzeltme yapıldı

--- largest_number.py
+++ largest_number.py
@@ -3,6 +3,8 @@ counter = 0
 while counter < 5:
     number = int(input("please enter a number: "))
     mylist.append(number)
-    counter = counter + 1
+    counter += 1
 sortedlist = mylist.sort()
 print(mylist[-1])
+
+
